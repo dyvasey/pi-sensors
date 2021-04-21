a681a7e805fdfe4d1f51582317ee7e221dba4e6b
Dylan Vasey <dyvasey@gmail.com>
2021-04-20 18:25:00 -0700

    Made 10 minute version for testing.

---
+++ pm25_log_short.py
@@ -0,0 +1,48 @@
+"""
+Script to log data from Adafruit PM 2.5 Sensor - test version for short time 
+"""
+
+import time
+import serial
+import pandas as pd
+from datetime import datetime
+from datetime import date
+
+# datetime object containing current date and time
+now = datetime.now()
+from adafruit_pm25.uart import PM25_UART
+
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
+pm25 = PM25_UART(uart)
+
+data_fields = ['pm10 standard','pm25 standard','pm100 standard',
+                   'pm10 env','pm25 env','pm100 env','particles 03um',
+                   'particles 05um','particles 10um','particles 25um',
+                   'particles 50um','particles 100um']
+
+columns = ['datetime'] + data_fields
+dataframe = pd.DataFrame(columns=columns)
+
+day = date.today()
+
+# Run 10 times
+n = 0
+while n<10:
+    # Run every 10 seconds
+    time.sleep(10)
+
+    try:
+        aqdata = pm25.read()
+    except RuntimeError:
+        print("Sensor reading failed")
+        continue
+    dt = datetime.now()
+    data = aqdata[data_fields]
+    row = pd.Series([dt,data])
+    dataframe.append(row)
+    n=n+1
+
+day = date.today()
+dataframe.to_csv(str(day)+'.csv')
+    
+
