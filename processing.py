7e616a92b1c674f0d5fe602aaf9822b1470ceae7
Dylan Vasey <dyvasey@gmail.com>
2021-04-22 23:01:53 -0700

    Began work on EPA AQI algorithim

--- processing.py
+++ processing.py
@@ -32,11 +32,25 @@ def aqi(directory = '/home/pi/Desktop/pm25-data/'):
     # Get every hour over past 12 hours
     try:
         rows = np.arange(last-72,last+1,6)
-        measurements = data.loc[rows,'pm25 standard']
-        return(measurements)
+        hour_measurements = data.loc[rows,'pm25 standard']
     
     except:
         print('Insufficient data available for AQI NowCast')
         print('Measurements Available: ',last)
-        return
+        return   
+    
+    all_measurements = data['pm25 standard']
+        
+    # Get max and min measurements
+    pm_max = all_measurements.max()
+    pm_min = all_measurements.min()
+    pm_range = pm_max-pm_min
+    # Scaled rate of change
+    scaled_roc = pm_range/pm_max
+    # Weight factor
+    weight_factor = 1-scaled_roc
+    
+    hour_measurements.reset_index()    
+        
+    return
 
