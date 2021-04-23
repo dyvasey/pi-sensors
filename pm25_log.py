6f484b5efd0d168245c0b7940aa67867bf387bcf
Dylan Vasey <dyvasey@gmail.com>
2021-04-22 20:48:17 -0700

    Added upload to Adafruit IO.

--- pm25_log.py
+++ pm25_log.py
@@ -7,10 +7,18 @@ import serial
 import pandas as pd
 from datetime import datetime
 from datetime import date
+
 from adafruit_pm25.uart import PM25_UART
+from Adafruit_IO import Client, RequestError, Feed
+
+# Separate .py file with Adafruit IO username and key
+import io_credentials
 
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 pm25 = PM25_UART(uart)
+
+aio = Client(io_credentials.username, io_credentials.key)
+feed = aio.feeds('pm-2-dot-5')
 
 data_fields = ['pm10 standard','pm25 standard','pm100 standard',
                    'pm10 env','pm25 env','pm100 env','particles 03um',
@@ -32,10 +40,17 @@ while True:
         time.sleep(600)
     
         try:
+            # Read data from the sensor
             aqdata = pm25.read()
+
         except RuntimeError:
             print("Sensor reading failed")
             continue
+        
+        # Send the PM 2.5 data to Adafruit IO
+        aio.send_data(feed.key, aqdata['pm25 standard'])
+        
+        # Organize and store data
         dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         data=[dt]
         for field in data_fields:
@@ -46,6 +61,7 @@ while True:
         dataframe = dataframe.append(row,ignore_index=True)
         path = '/home/pi/Desktop/pm25-data/' + str(day) + '.csv'
         dataframe.to_csv(path,index=False)
+        
 
     
 
