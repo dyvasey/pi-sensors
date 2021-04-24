f6e7661b054081a16e58839152b0631a3f42be97
Dylan Vasey <dyvasey@gmail.com>
2021-04-24 13:22:12 -0700

    Added script to run to continuously nowcast.

---
+++ nowcast.py
@@ -0,0 +1,30 @@
+"""
+Nowcast using most recent data
+"""
+
+import processing
+import time
+
+from Adafruit_IO import Client, RequestError, Feed
+
+# Separate .py file with Adafruit IO username and key
+import io_credentials
+
+aio = Client(io_credentials.username, io_credentials.key)
+feed = aio.feeds('aqi-nowcast')
+
+while True:
+    # Run every 10 minutes
+    time.sleep(600)
+
+    try:
+        # Try to calculate AQI
+        pm25,aqi,level = processing.aqi_nowcast()  
+
+    except:
+        print("Can't Calculate AQI")
+        continue
+    
+    # Send the PM 2.5 data to Adafruit IO
+    aio.send_data(feed.key, aqi)
+    
