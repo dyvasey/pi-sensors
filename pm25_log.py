1623605cf66557b962548e4f1660521e492fd24c
Dylan Vasey <dyvasey@gmail.com>
2021-04-25 18:47:51 -0700

    Updated pm25_log to run only once.

--- pm25_log.py
+++ pm25_log.py
@@ -2,7 +2,6 @@
 Script to log data from Adafruit PM 2.5 Sensor
 """
 
-import time
 import serial
 import pandas as pd
 from datetime import datetime
@@ -10,7 +9,7 @@ from datetime import date
 from os import path
 
 from adafruit_pm25.uart import PM25_UART
-from Adafruit_IO import Client, RequestError, Feed
+from Adafruit_IO import Client
 
 # Separate .py file with Adafruit IO username and key
 import io_credentials
@@ -28,51 +27,42 @@ data_fields = ['pm10 standard','pm25 standard','pm100 standard',
 
 columns = ['datetime'] + data_fields
 
-# Set to run indefinitely after setup above
-while True:
-    # Get current date for next CSV file
-    day = date.today()
-    pth = '/home/pi/Desktop/pm25-data/' + str(day) + '.csv'
-    
-    # Get existing dataframe if it exists
-    if path.exists(pth):
-        dataframe = pd.read_csv(pth)
-    else:
-        # Reset dataframe to empty
-        dataframe = pd.DataFrame(columns=columns)
-    
-    # Continue running until next day
-    while day==date.today():
-        # Run every 10 minutes
-        time.sleep(600)
-    
-        try:
-            # Read data from the sensor
-            aqdata = pm25.read()
+# Get current date
+day = date.today()
+pth = '/home/pi/Desktop/pm25-data/' + str(day) + '.csv'
 
-        except RuntimeError:
-            print("Sensor reading failed")
-            continue
-        
-        except serial.SerialException:
-            print("Sensor failed to connect")
-            continue
-        
-        # Send the PM 2.5 data to Adafruit IO
-        aio.send_data(feed.key, aqdata['pm25 standard'])
-        
-        # Organize and store data
-        dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        data=[dt]
-        for field in data_fields:
-            value = aqdata[field]
-            data.append(value)
-        print(data)
-        row = pd.Series(data,index=columns)
-        dataframe = dataframe.append(row,ignore_index=True)
-        
-        dataframe.to_csv(pth,index=False)
-        
+# Get existing dataframe if it exists
+if path.exists(pth):
+    dataframe = pd.read_csv(pth)
+else:
+    # Start empty dataframe
+    dataframe = pd.DataFrame(columns=columns)
+
+try:
+    # Read data from the sensor
+    aqdata = pm25.read()
+
+except RuntimeError:
+    print("Sensor reading failed")
+
+except serial.SerialException:
+    print("Sensor failed to connect")
+
+# Send the PM 2.5 data to Adafruit IO
+aio.send_data(feed.key, aqdata['pm25 standard'])
+
+# Organize and store data
+dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+data=[dt]
+for field in data_fields:
+    value = aqdata[field]
+    data.append(value)
+print(data)
+row = pd.Series(data,index=columns)
+dataframe = dataframe.append(row,ignore_index=True)
+
+dataframe.to_csv(pth,index=False)
+    
 
     
 
