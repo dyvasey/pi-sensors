366097c98edccdbd7cb07a2fb874ae395572f9d1
Dylan Vasey <dyvasey@gmail.com>
2021-04-25 19:02:10 -0700

    Removed background process aspect of bash script.

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-nohup sudo -b python3 pm25_log.py >> logs/pm25_log.log 2>&1
-nohup sudo -b python3 nowcast.py >> logs/nowcast.log 2>&1
+sudo python3 pm25_log.py >> logs/pm25_log.log 2>&1
+sudo python3 nowcast.py >> logs/nowcast.log 2>&1
 
-echo "Initiated data logging and AQI Nowcasting."
+echo "Logged PM2.5 data and AQI Nowcast."
 
 
