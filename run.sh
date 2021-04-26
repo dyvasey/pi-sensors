59e0dd758de666857333c3b3baf9208f570e6533
Dylan Vasey <dyvasey@gmail.com>
2021-04-25 18:33:10 -0700

    Fixed typo in run.sh and updated gitignore.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 nohup sudo -b python3 pm25_log.py >> logs/pm25_log.log 2>&1
-nohup sudo -b python3 nowcasting.py >> logs/nowcasting.log 2>&1
+nohup sudo -b python3 nowcast.py >> logs/nowcast.log 2>&1
 
 echo "Initiated data logging and AQI Nowcasting."
 
