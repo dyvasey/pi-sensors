eb9ad7188bad785988a8943845848923dc0bc726
Dylan Vasey <dyvasey@gmail.com>
2021-05-05 21:25:39 -0700

    Added backlight to charlcd test.

--- tests/charlcd_test.py
+++ tests/charlcd_test.py
@@ -11,13 +11,16 @@ lcd_d7 = digitalio.DigitalInOut(board.D27)
 lcd_d6 = digitalio.DigitalInOut(board.D22)
 lcd_d5 = digitalio.DigitalInOut(board.D24)
 lcd_d4 = digitalio.DigitalInOut(board.D25)
+lcd_backlight = digitalio.DigitalInOut(board.D4)
 
 lcd_columns = 16
 lcd_rows = 2
 
 import adafruit_character_lcd.character_lcd as characterlcd
 lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
-                                      lcd_d7, lcd_columns, lcd_rows)
+                                      lcd_d7, lcd_columns, lcd_rows,
+                                      lcd_backlight)
 
+lcd.backlight = True
 lcd.message = "I Love\nMoop"
 
