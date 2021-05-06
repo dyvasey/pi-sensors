4c060fa559f1ac8f0b51d3c07d185e2b1f1575d6
Dylan Vasey <dyvasey@gmail.com>
2021-05-05 21:16:29 -0700

    Added backlight turnoff to processing

--- processing.py
+++ processing.py
@@ -141,13 +141,22 @@ def disp_aqi(pm25,aqi):
     lcd_d6 = digitalio.DigitalInOut(board.D22)
     lcd_d5 = digitalio.DigitalInOut(board.D24)
     lcd_d4 = digitalio.DigitalInOut(board.D25)
+    lcd_backlight = digitalio.DigitalInOut(board.D4)
 
     lcd_columns = 16
     lcd_rows = 2
     
     lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5,
                                           lcd_d6,lcd_d7, lcd_columns,
-                                          lcd_rows)
+                                          lcd_rows,lcd_backlight)
+    
+    # LCD on from 9 AM to 10 PM
+    now = datetime.now()
+    if 9<=now.hour<22:
+        lcd.backlight = True
+    else:
+        lcd.backlight = False
+    
     
     lcd.message = "PM2.5: " +str(pm25) + "\nAQI: " + str(aqi)
     return
